7634871e51486aa3c3049985bef3869104ac3aa6
Rai Butera <rai@rbutera.com>
2019-12-04 13:24:42 +0000

    subtitle now staggers changes, now need to animate

--- src/components/subtitle/subtitle.js
+++ src/components/subtitle/subtitle.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { Text, Line } from './subtitle.css';
+import { useObservable } from 'rxjs-hooks';
+import { interval } from 'rxjs';
+import { map, withLatestFrom } from 'rxjs/operators';
+
+function shuffle(input = []) {
+  if (!input || input.length < 2) {
+    return input;
+  }
+
+  const [current, ...rest] = input;
+  const randomIndex = Math.floor(Math.random() * rest.length);
+  const next = rest[randomIndex];
+  const remaining = rest.filter(x => x !== next);
+  return [next, current, ...remaining];
+}
 
 const MorphingPhrase = ({ children }) => {
   return <>{children}</>;
 };
 
-const Segment = ({ before, children }) => {
+const Segment = ({ before, children, options }) => {
   return (
     <Line>
       {before}
@@ -15,11 +30,28 @@ const Segment = ({ before, children }) => {
   );
 };
 
-const Subtitle = ({ as = 'span', size = 'medium' }) => {
+const Subtitle = ({ as = 'span', size = 'medium', options }) => {
+  const { first, second } = options;
+  const [firstOptions, setFirstOptions] = useState(first);
+  const [secondOptions, setSecondOptions] = useState(second);
+
+  useEffect(() => {
+    const i = setInterval(() => {
+      setFirstOptions(shuffle(firstOptions));
+      setTimeout(() => {
+        setSecondOptions(shuffle(secondOptions));
+      }, 1000);
+    }, 5000);
+
+    return () => {
+      clearInterval(i);
+    };
+  }, []);
+
   return (
     <Text as={as} size={size}>
-      <Segment before="a ">optionA</Segment>
-      <Segment before="of ">optionB</Segment>
+      <Segment before="a ">{firstOptions[0]}</Segment>
+      <Segment before="of ">{secondOptions[0]}</Segment>
     </Text>
   );
 };
