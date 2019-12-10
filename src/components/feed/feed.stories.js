938ed73bd6e58d5c3a0de707e247b9288c8889f9
Rai Butera <rai@rbutera.com>
2019-12-10 12:08:33 +0000

    imported code from prototype for feed

---
+++ src/components/feed/feed.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { action } from '@storybook/addon-actions';
+import Feed from './feed';
+
+export default {
+  title: 'Feed',
+};
+
+export const normal = () => <Feed />;
--- src/components/feed/header.stories.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { action } from '@storybook/addon-actions';
-import Header from './header';
-
-export default {
-  title: 'Header',
-};
-
-export const normal = () => <Header as="h1">Latest Recommendations</Header>;
