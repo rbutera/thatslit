c53cc46284d23a59849a504d5b67332a38ebd703
Rai Butera <rai@rbutera.com>
2019-12-17 12:31:06 +0000

    successfully got the grid to display recommendations

--- src/components/masonry/masonry.tsx
+++ src/components/masonry/masonry.tsx
@@ -1,15 +1,40 @@
 import * as React from 'react'
 import { Grid } from 'mauerwerk'
 import data from './data'
-import './masonry.css'
-import Recommendation from '../recommendation/recommendation'
 
-export const Masonry = () => {
+import Recommendation from '../recommendation/recommendation'
+import styled from 'styled-components'
+import tw from 'tailwind.macro'
+import { Card } from '../card/card'
+
+function normalizeFields(fields) {
+  const { Name, Tagline, Picture, Subcat } = fields
+
+  const result = {
+    name: Name,
+    tagline: Tagline,
+    category: Subcat,
+    picture:
+      Picture && Picture[0] ? Picture[0]['thumbnails']['large']['url'] : '',
+  }
+
+  return result
+}
+
+const Cell = styled.div`
+  ${tw`w-full h-full`};
+`
+
+const Minimized = styled.div`
+  ${tw`w-full h-full`};
+`
+
+export const Masonry = ({}) => {
   const RECS = [
-    { key: 'a', title: 'foo', height: '120px' },
-    { key: 'b', title: 'bar', height: '120px' },
-    { key: 'c', title: 'om', height: '120px' },
-    { key: 'd', title: 'kar', height: '120px' },
+    { key: 'a', title: 'foo', height: '120px', size: 'large' },
+    { key: 'b', title: 'bar', height: '120px', size: 'small' },
+    { key: 'c', title: 'om', height: '120px', size: 'large' },
+    { key: 'd', title: 'kar', height: '120px', size: 'small' },
   ]
   return (
     <Grid
@@ -24,20 +49,15 @@ export const Masonry = () => {
       columns={3}
     >
       {(data, maximized, toggle) => (
-        <div
-          className="cell"
-          style={{ backgroundImage: data.css }}
-          onClick={toggle}
-        >
-          {maximized && (
-            <div className="details">
-              <div className="circle" style={{ background: data.css }} />
-              <h1>{data.name}</h1>
-              <p>{data.description}</p>
-            </div>
-          )}
-          {!maximized && <div className="default">{data.name}</div>}
-        </div>
+        <Cell style={{ backgroundImage: data.css }} onClick={toggle}>
+          <Recommendation
+            name={data.name}
+            tagline={data.description}
+            category={data.name}
+            picture="https://source.unsplash.com/random/1024x1024"
+            variant="large"
+          ></Recommendation>
+        </Cell>
       )}
     </Grid>
   )
