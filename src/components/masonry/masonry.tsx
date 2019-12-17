521fcdd2b7cf95e6ce175a2acb2e447bbd845738
Rai Butera <rai@rbutera.com>
2019-12-17 12:04:24 +0000

    imported masonry from example, it works

--- src/components/masonry/masonry.tsx
+++ src/components/masonry/masonry.tsx
@@ -1,5 +1,44 @@
 import * as React from 'react'
+import { Grid } from 'mauerwerk'
+import data from './data'
+import './masonry.css'
+import Recommendation from '../recommendation/recommendation'
 
 export const Masonry = () => {
-  return <div>Masonry</div>
+  const RECS = [
+    { key: 'a', title: 'foo', height: '120px' },
+    { key: 'b', title: 'bar', height: '120px' },
+    { key: 'c', title: 'om', height: '120px' },
+    { key: 'd', title: 'kar', height: '120px' },
+  ]
+  return (
+    <Grid
+      className="grid"
+      // Arbitrary data, should contain keys, possibly heights, etc.
+      data={data}
+      // Key accessor, instructs grid on how to fet individual keys from the data set
+      keys={d => d.name}
+      // Can be a fixed value or an individual data accessor
+      heights={d => d.height}
+      // Number of columns
+      columns={3}
+    >
+      {(data, maximized, toggle) => (
+        <div
+          className="cell"
+          style={{ backgroundImage: data.css }}
+          onClick={toggle}
+        >
+          {maximized && (
+            <div className="details">
+              <div className="circle" style={{ background: data.css }} />
+              <h1>{data.name}</h1>
+              <p>{data.description}</p>
+            </div>
+          )}
+          {!maximized && <div className="default">{data.name}</div>}
+        </div>
+      )}
+    </Grid>
+  )
 }
